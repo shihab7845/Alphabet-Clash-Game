b054cf09842dc2dcc11b4602ba6c7eefe3671de9
unknown <tarekurrahman7845@gmail.com>
2024-04-24 02:34:19 +0600

    alpha crash game

---
+++ app.js
@@ -0,0 +1,34 @@
+// add and remove hidden class( change the screen)
+
+const playNow = document.getElementById("play_now").addEventListener('click',function(){
+   
+//  /** 1 */   
+//   const homeScreen = document.getElementById('home_screen');
+//     homeScreen.classList.add("hidden")
+
+//   /** 2 */  
+//   const KEYBOARD = document.getElementById('keyboard');
+//     KEYBOARD.classList.remove('hidden')
+
+showHide('home_screen','keyboard')
+
+ /** 3 */
+ getRandomAlphabet()
+
+  /** 4 */
+  // setAlphabetsBackground(randomAlphabet)
+
+
+})
+
+   
+
+
+
+
+
+
+
+
+
+
---
+++ utility.js
@@ -0,0 +1,126 @@
+
+let updateScore = 0;
+function handleKeyboardButtonPress(event) {
+    // console.log(event.key);
+    const playerpressedkey = event.key;
+    const ScreenShowedKey = document.getElementById("random_alphabets").innerText;
+    const lowerString = ScreenShowedKey.toLowerCase()
+    console.log(lowerString);
+   
+    // let newScore = convertIntoInt("current-score");
+
+    if (playerpressedkey === lowerString) {
+
+        removeAlphabetsBackground(lowerString);
+        getRandomAlphabet()
+
+        // let newScore = convertIntoInt("current-score");
+        // const updateScore = newScore + 1;
+        updateScore++
+
+        disPlay('current-score', updateScore);
+
+    }
+    else {
+        console.log("you are not pressed correct button");
+
+        const lifeRemain = convertIntoInt('current-life');
+
+        const newLife = lifeRemain - 1;
+
+        if (newLife === 0) {
+           hideScreen("final-score");
+           showScreen('keyboard');
+            disPlay('final-score',updateScore)
+        } else {
+            disPlay('current-life', newLife);
+
+        }
+
+    }
+}
+// event fire when clicked 
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
+// create random alphabets and show it in screen
+function getRandomAlphabet() {
+
+    const alphabetsStrings = "abcdefghijklmnopqrstuvwxyz";
+    const alphabetsString = alphabetsStrings.split('');
+
+
+    //  create random number between 0 to 25
+    const numbers = Math.round(Math.random() * 25)
+
+    // console.log(`random number is ${numbers} and alphabets is ${alphabetsString[numbers]}`);
+
+    // random alphabets
+    const randomAlphabet = alphabetsString[numbers];
+    console.log(randomAlphabet);
+
+    // show alphabets in screen 
+    const randomAlphabets = document.getElementById('random_alphabets');
+    randomAlphabets.innerText = randomAlphabet;
+
+    setAlphabetsBackground(randomAlphabet)
+
+
+    return randomAlphabet;
+}
+
+// set background color
+function setAlphabetsBackground(element) {
+    const alphabetBackground = document.getElementById(element)
+    alphabetBackground.classList.add('bg-orange-400')
+}
+
+// remove background color
+function removeAlphabetsBackground(element) {
+    const alphabetBackground = document.getElementById(element)
+    alphabetBackground.classList.remove('bg-orange-400')
+}
+
+function disPlay(event, value) {
+    
+    const element = document.getElementById(event);
+    
+    element.innerText = value;
+}
+
+// convert into presINT
+function convertIntoInt(event) {
+    const element = document.getElementById(event);
+    const elementText = element.innerText;
+    const convertedElement = parseInt(elementText);
+    return convertedElement;
+}
+
+// show one screen at the same the hide another one 
+
+function showHide(event, event2) {
+
+    const element = document.getElementById(event);
+    element.classList.add("hidden");
+
+    const element2 = document.getElementById(event2);
+    element2.classList.remove("hidden");
+}
+// show screen 
+function showScreen(event){
+    const element = document.getElementById(event);
+    element.classList.add("hidden");
+}
+// hide screen
+function hideScreen(event){
+    const element2 = document.getElementById(event);
+    element2.classList.remove("hidden");
+}
+
+// play again function
+function playAgain(){
+    
+    hideScreen("final-score");
+    showScreen("keyboard");
+    getRandomAlphabet();
+}
+
